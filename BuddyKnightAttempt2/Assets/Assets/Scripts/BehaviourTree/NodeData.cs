32cc6cb59c3762f59c912770af5b71e9917ae0b2
Man-with-a-plan <121805053+Man-with-a-plan@users.noreply.github.com>
2025-11-27 12:41:54 +0200

    No need for attempt 2

--- BuddyKnightAttempt2/Assets/Assets/Scripts/BehaviourTree/NodeData.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-public class NodeData
-{
-    public State State = State.Running;
-    public bool Stopped = true;
-}
--- BuddyKnightAttempt2/Assets/Assets/Scripts/BehaviourTree/State.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-public enum State
-{
-    Running,
-    Failure,
-    Success
-}
